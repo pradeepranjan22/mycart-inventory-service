7ec9c0a85c595855d6fbfc52064f19af80031ad8
pradeepranjan22 <103499157+pradeepranjan22@users.noreply.github.com>
2025-03-24 08:55:37 +0530

    updated

--- src/main/java/com/secor/restroservice/inventoryServiceApplication.java
+++ src/main/java/com/secor/restroservice/inventoryServiceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestroServiceApplication {
+public class inventoryServiceApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(RestroServiceApplication.class, args);
+        SpringApplication.run(inventoryServiceApplication.class, args);
 
     }
 
